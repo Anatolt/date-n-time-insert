6d584d379942bc904877aaf9ec3ee9b7bbbd1daf
Anatoly Tarasenko <fm.ksbw@gmail.com>
2015-04-01 01:08:08 +0500

    It works!
    If you want to just copy - change hotkeys.

--- date-n-time-insert.py
+++ date-n-time-insert.py
@@ -2,17 +2,25 @@
 # http://schurpf.com/python/python-hotkey-module/
 import time
 import pyhk
+from Tkinter import Tk
 
 
 def fun():
     now = time.strftime("%Y-%m-%d %H:%M:%S")
+    r = Tk()
+    r.withdraw()
+    r.clipboard_clear()
+    r.clipboard_append(now)
+    r.destroy()
     print now
 
 # create pyhk class instance
 hot = pyhk.pyhk()
 
 # add hotkey
-hot.addHotkey(['Ctrl', 'Alt', 'D'], fun)
+# if you want just copy uncoment this 
+# hot.addHotkey(['Ctrl', 'Alt' ,'D'], fun)
+hot.addHotkey(['Ctrl', 'V'], fun)
 
 # start looking for hotkey.
 hot.start()
