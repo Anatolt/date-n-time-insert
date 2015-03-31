89362ad6b51f1be608126190defcd0d1e13d61d0
Anatoly Tarasenko <fm.ksbw@gmail.com>
2015-03-31 23:00:26 +0500

    Update date-n-time-insert.py

--- date-n-time-insert.py
+++ date-n-time-insert.py
@@ -3,3 +3,4 @@ import time
 now = time.strftime("%Y-%m-%d %H:%M")
 print now
 # http://stackoverflow.com/questions/16615087/python-how-to-create-a-global-hotkey-on-windows-with-3-arguments
+# http://schurpf.com/python/python-hotkey-module/
